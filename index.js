a83676c9e9c52a4256659a8ab4f9f184fc2c0123
Dilip Rai <janeerrai@gmail.com>
2024-04-26 23:52:38 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import React from "react";
 import App from "./src/App";
 import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(<App />);
