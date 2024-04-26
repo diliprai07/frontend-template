653e599513a430e6c36dd277706e0f98302bce4e
raidilip <39137842+diliprai07@users.noreply.github.com>
2024-04-27 01:40:58 +0545

    Resume generator landing page design is started

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import * as React from "react";
 
 const HomePage = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="home_container">
+      <div className="main-info-container">
+        <h1>Best way to build your resume</h1>
+        <h4>
+          One of the best solutions for the working professionals, to build
+          resume and land into best job.
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
