32ecf40e7397d3a4d483e2a5ddc7ba0f2a05d1e1
raidilip <39137842+diliprai07@users.noreply.github.com>
2024-05-15 23:12:47 +0545

    resume detail view

--- src/components/resume-detail/resume-detail.tsx
+++ src/components/resume-detail/resume-detail.tsx
@@ -4,8 +4,11 @@ const ResumeDetail = () => {
   return (
     <div className="resume-detail">
       <span className="resume-header">Dilip Rai</span>
-      <span className="resume-header">Dilip Rai</span>
-      <span className="resume-header">Dilip Rai</span>
+      <span className="designation">Software Engineer</span>
+      <div>
+        <a>dilip@gmail.com</a>
+        <a>linkedin.com/diliprai</a>
+      </div>
     </div>
   );
 };
